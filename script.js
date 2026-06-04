ed290441f9dc5aa8adfb0f48681a24ad0ad1f192
Valérie Lafouasse <valerie.lafouasse2026@campus-eni.fr>
2026-06-04 09:00:44 +0200

    Modification index.html, promo.json, script.js

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
 /*mode sombre*/
 const select = document.getElementById ('theme')
-select.addEventListener ('change', function (){
+if(select) {
+    select.addEventListener ('change', function (){
 if (select.value === 'sombre') {
     document.body.classList.add('sombre')
 } else {
     document.body.classList.remove('sombre')
 }
 })
+}
 
 /*mode carte*/
+
+
 const choixListe = document.getElementById ('affichage-liste')
 const choixCarte = document.getElementById ('affichage-carte')
 
+/* je verifie que l'élément existe avant d'ajouter le listener sinon ça bloque le script*/
+if (choixListe) {
 choixListe.addEventListener ('change', function (){
     localStorage.setItem('affichage','liste')
 })
+}
 
+if (choixCarte) {
 choixCarte.addEventListener ('change', function (){
     localStorage.setItem('affichage','carte')
 })
-  
+}  
 
-
-
-/* menu burger*/
+/* menu burger
 const burger = document.querySelector(".burger")
 const menu = document.querySelector (".menu")
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("active")
 })
+*/ 
+
+
+/* récupérer les données de promo.json*/
+
+fetch ('promo.json')
+.then(response => response.json())
+.then(data => afficherData(data.apprenants))
+
+function afficherData (data){
+    /* cibler tbody  avant d'écrire dedans*/
+   const tbody = document.getElementById('tbody-promo')
+    
+/* boucler sur data avec foreach et dans la boucle je crée la ligne et je l'injecte dans html*/
+   data.forEach(personne => {
+        const ligne = `<tr>
+    <td>${personne.nom}</td>
+    <td>${personne.prenom}</td>
+    <td>${personne.ville}</td>
+    <td><a href="">Détails</a></td>
+</tr>`
+        tbody.innerHTML += ligne
+        
+}) 
+}
+
