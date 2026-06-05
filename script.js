461054ea42d7f1c89e6ba857a0244b2fbc6dc6be
Valérie Lafouasse <valerie.lafouasse2026@campus-eni.fr>
2026-06-05 14:20:29 +0200

    ADD: modification des fichiers html index, informations, preferences et script.js et style.css

--- script.js
+++ script.js
@@ -1,42 +1,78 @@
 /*mode sombre*/
 const select = document.getElementById ('theme')
+
+/* Appliquer le thème au chargement de la page */
+const prefTheme = localStorage.getItem('theme')
+if (prefTheme === 'sombre') {
+    document.body.classList.add('sombre')
+}
+if (prefTheme && select) {
+    select.value = prefTheme
+}
+/*écouter les changements
 if(select) {
     select.addEventListener ('change', function (){
-if (select.value === 'sombre') {
-    document.body.classList.add('sombre')
-} else {
-    document.body.classList.remove('sombre')
-}
-})
-}
+       if (select.value === 'sombre') {
+       document.body.classList.add('sombre')
+       } else {
+       document.body.classList.remove('sombre')
+       }
+    })
+}*/
 
-/*mode carte*/
-
+/*affichage carte - déclarations */
 
 const choixListe = document.getElementById ('affichage-liste')
 const choixCarte = document.getElementById ('affichage-carte')
+const btnEnregistrer = document.getElementById('btn-enregistrer')
 
-/* je verifie que l'élément existe avant d'ajouter le listener sinon ça bloque le script*/
-if (choixListe) {
-choixListe.addEventListener ('change', function (){
-    localStorage.setItem('affichage','liste')
-})
-}
 
+/*affichage des préférences au chargement de la page index*/
+const prefAffichage = localStorage.getItem('affichage')
+if (prefAffichage === 'carte') {
+    const scroller = document.querySelector ('.scroller')
+    if (scroller) scroller.classList.add('carte')
+    if (choixCarte) choixCarte.checked = true
+    }else {
+      if (choixListe) choixListe.checked= true
+    }
+
+/*Sur Index.html, enregistrer le choix de l'affichage au 'change' du radio bouton >localStorage et classe CSS
+/* Vérifier que l'élément existe avant d'ajouter le listener sinon ça bloque le script*/
 if (choixCarte) {
 choixCarte.addEventListener ('change', function (){
+     console.log('clic carte détecté')
     localStorage.setItem('affichage','carte')
+   /*cibler .scroller  et ajouter la classe carte*/ 
+    document.querySelector('.scroller').classList.add('carte')
 })
 }  
 
-/* menu burger
-const burger = document.querySelector(".burger")
-const menu = document.querySelector (".menu")
-
-burger.addEventListener("click", () => {
-    menu.classList.toggle("active")
+if (choixListe) {
+choixListe.addEventListener ('change', function (){
+    localStorage.setItem('affichage','liste')
+    document.querySelector('.scroller').classList.remove('carte')
 })
-*/ 
+}
+
+/*Sur preferences.html, enregistrer le choix d'affichage radio bouton> bouton enregistrer > localStorage (le code ne s'execute que si la page html contient le bouton enregistrer)*/
+if (btnEnregistrer) {
+    btnEnregistrer.addEventListener('click', function() {
+        localStorage.setItem('theme', select.value)
+        if (select.value === 'sombre') {
+            document.body.classList.add('sombre')
+        } else {
+            document.body.classList.remove('sombre')
+        }
+        if (choixCarte.checked) {
+        localStorage.setItem('affichage', 'carte')
+        } else {
+        localStorage.setItem('affichage', 'liste')
+        }
+        document.getElementById('message-confirmation').innerText = 'Vos préférences ont bien été enregistrées !'
+    })
+}
+    
 
 
 /* récupérer les données de promo.json*/
@@ -48,17 +84,35 @@ fetch ('promo.json')
 function afficherData (data){
     /* cibler tbody  avant d'écrire dedans*/
    const tbody = document.getElementById('tbody-promo')
+   const vueCarte = document.getElementById('vue-carte')
     
-/* boucler sur data avec foreach et dans la boucle je crée la ligne et je l'injecte dans html*/
+/* boucler sur data avec foreach et dans la boucle créer la ligne et l'injecter dans html*/
    data.forEach(personne => {
         const ligne = `<tr>
     <td>${personne.nom}</td>
     <td>${personne.prenom}</td>
     <td>${personne.ville}</td>
     <td><a href="">Détails</a></td>
-</tr>`
+    </tr>`
         tbody.innerHTML += ligne
-        
+     /*Injecter ligne et carte en même temps dans le for each: */
+        const vignette = `<div class="vignette">
+     <h2>${personne.nom} ${personne.prenom}</h2>
+     <p>${personne.ville}</p>
+     <a href="">Détails</a>
+     </div>`
+        vueCarte.innerHTML += vignette   
 }) 
 }
+
+
+
+/* menu burger
+const burger = document.querySelector(".burger")
+const menu = document.querySelector (".menu")
+
+burger.addEventListener("click", () => {
+    menu.classList.toggle("active")
+})
+*/
 
